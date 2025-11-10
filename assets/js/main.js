be280e9dde8d512c12932f2c18d610028d2a6d20
Zakirul Islam <zakirul011@gmail.com>
2025-11-10 19:09:23 +0600

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+(function ($) {
+	"use strict";
+  //========== PRODUCT SLIDER ==========>
+	$('.testimony-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		centerMode: true,
+		centerPadding: '20%',
+		arrows: true,
+		prevArrow: '<button type="button" class="slick-prev"></button>',
+		nextArrow: '<button type="button" class="slick-next"></button>',
+
+		// responsive: [{
+		// 		breakpoint: 1400,
+		// 		settings: {
+		// 			slidesToShow: 2,
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow: 2,
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 375,
+		// 		settings: {
+		// 			slidesToShow: 1,
+		// 		}
+		// 	}
+		// ]
+	});
+  //========== PRODUCT SLIDER// ==========>
+
+})(jQuery);
